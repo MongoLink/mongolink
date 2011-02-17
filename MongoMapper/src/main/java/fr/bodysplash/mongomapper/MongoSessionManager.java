fd39023d7229228c14877a935a0639b140428e13
BodySplash <bodysplash@gmail.com>
2011-02-17 19:23:33 +0100

    idea fail

---
+++ MongoMapper/src/main/java/fr/bodysplash/mongomapper/MongoSessionManager.java
@@ -0,0 +1,39 @@
+package fr.bodysplash.mongomapper;
+
+import com.mongodb.DB;
+import fr.bodysplash.mongomapper.mapper.ContextBuilder;
+import fr.bodysplash.mongomapper.mapper.MapperContext;
+
+public class MongoSessionManager {
+
+    private static String dbName;
+    private MapperContext mapperContext;
+    private DbFactory dbFactory = new DbFactory();
+
+    public static MongoSessionManager create(ContextBuilder contextBuilder, String dbName) {
+        MongoSessionManager.dbName = dbName;
+        return new MongoSessionManager(contextBuilder.createContext());
+    }
+
+    private MongoSessionManager(MapperContext mapperContext) {
+        this.mapperContext = mapperContext;
+    }
+
+    public MongoSession createSession() {
+        MongoSession mongoSession = new MongoSession(getDb());
+        mongoSession.setMappingContext(mapperContext);
+        return mongoSession;
+    }
+
+    private DB getDb() {
+        return dbFactory.get(dbName);
+    }
+
+    public MapperContext getMapperContext() {
+        return mapperContext;
+    }
+
+    public void setDbFactory(DbFactory dbFactory) {
+        this.dbFactory = dbFactory;
+    }
+}
