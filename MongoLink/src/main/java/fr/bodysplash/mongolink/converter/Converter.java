c9cf6229b1aa9c75e1534f4c71b6e03ed69887a5
David Dumon <dumon.david@gmail.com>
2011-04-26 14:23:20 +0200

    DateTime converter

--- MongoLink/src/main/java/fr/bodysplash/mongolink/converter/Converter.java
+++ MongoLink/src/main/java/fr/bodysplash/mongolink/converter/Converter.java
@@ -1,5 +1,7 @@
 package fr.bodysplash.mongolink.converter;
 
+import org.joda.time.DateTime;
+
 import java.lang.reflect.Method;
 
 public abstract class Converter {
@@ -9,6 +11,9 @@ public abstract class Converter {
     public static Converter forMethod(Method method) {
         if (method.getReturnType().isEnum()) {
             return new EnumConverter(method.getReturnType());
+        }
+        if (method.getReturnType().isInstance(new DateTime())) {
+            return new DateTimeConverter();
         }
         return PRIMITIVE_CONVERTER;
     }
---
+++ MongoLink/src/main/java/fr/bodysplash/mongolink/converter/DateTimeConverter.java
@@ -0,0 +1,16 @@
+package fr.bodysplash.mongolink.converter;
+
+import org.joda.time.DateTime;
+
+public class DateTimeConverter extends Converter {
+
+    @Override
+    public Object toDbValue(final Object value) {
+        return ((DateTime) value).getMillis();
+    }
+
+    @Override
+    public Object fromDbValue(final Object value) {
+        return new DateTime(Long.getLong(value.toString()));
+    }
+}
