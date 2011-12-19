997bc0d59c1cfe3171b97ba2bb505c473b277c6a
bodysplash <bodysplash@gmail.com>
2011-12-19 15:57:43 +0100

    il est possible maintenant de chercher le mapping dans des jar

--- MongoLink/src/main/java/fr/bodysplash/mongolink/domain/mapper/ContextBuilder.java
+++ MongoLink/src/main/java/fr/bodysplash/mongolink/domain/mapper/ContextBuilder.java
@@ -4,15 +4,17 @@ import com.google.common.collect.Lists;
 import fr.bodysplash.mongolink.MongoLinkError;
 import org.apache.log4j.Logger;
 
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
+import java.net.JarURLConnection;
 import java.net.URL;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 public class ContextBuilder {
-
-    private static final Logger LOGGER = Logger.getLogger(ContextBuilder.class);
-
-    private final String packageToScan;
 
     public ContextBuilder(String packageToScan) {
         this.packageToScan = packageToScan;
@@ -21,13 +23,14 @@ public class ContextBuilder {
     public MapperContext createContext() {
         MapperContext result = new MapperContext();
         try {
-            Iterable<Class> classes = getClasses();
+            Iterable<Class> classes = getCandidateClasses();
             for (Class currentClass : classes) {
                 if (isAMap(currentClass)) {
                     ClassMap<?> mapping = (ClassMap<?>) currentClass.newInstance();
                     mapping.buildMapper(result);
                 }
             }
+
             LOGGER.debug("Done scanning package");
         } catch (Exception e) {
             throw new MongoLinkError("Error scanning package", e);
@@ -40,37 +43,88 @@ public class ContextBuilder {
         return ClassMap.class.isAssignableFrom(currentClass) && !SubclassMap.class.isAssignableFrom(currentClass);
     }
 
-    private Iterable<Class> getClasses() throws ClassNotFoundException, IOException {
-        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        String path = packageToScan.replace('.', '/');
-        Enumeration<URL> resources = classLoader.getResources(path);
-        List<File> dirs = Lists.newArrayList();
-        while (resources.hasMoreElements()) {
-            URL resource = resources.nextElement();
-            dirs.add(new File(resource.getFile()));
-        }
+    private Iterable<Class> getCandidateClasses() throws ClassNotFoundException, IOException {
         List<Class> classes = Lists.newArrayList();
-        for (File directory : dirs) {
-            classes.addAll(findClasses(directory, packageToScan));
-        }
-
-        return classes;
-    }
-
-
-    private List<Class> findClasses(File directory, String packageName) throws ClassNotFoundException {
-        List<Class> classes = new ArrayList<Class>();
-        if (!directory.exists()) {
-            return classes;
-        }
-        File[] files = directory.listFiles();
-        for (File file : files) {
-            if (file.isDirectory()) {
-                classes.addAll(findClasses(file, packageName + "." + file.getName()));
-            } else if (file.getName().endsWith(".class")) {
-                classes.add(Class.forName(packageName + '.' + file.getName().substring(0, file.getName().length() - 6)));
+        for (URL url : getResources()) {
+            if(isAJar(url)) {
+                classes.addAll(findClassesFromJar(url));
+            } else {
+                classes.addAll(findClassesFromDirectory(new File(url.getFile()), packageToScan));
             }
         }
         return classes;
     }
+
+    private boolean isAJar(URL url) {
+        return "jar".equals(url.getProtocol());
+    }
+
+    private List<URL> getResources() throws IOException {
+        Enumeration<URL> resources = classLoader().getResources(packageToDirectory());
+        List<URL> dirs = Lists.newArrayList();
+        while (resources.hasMoreElements()) {
+            URL resource = resources.nextElement();
+            dirs.add(resource);
+        }
+        return dirs;
+    }
+
+    private String packageToDirectory() {
+        return packageToScan.replace(".", File.separator);
+    }
+
+    private ClassLoader classLoader() {
+        return Thread.currentThread().getContextClassLoader();
+    }
+
+    private List<Class> findClassesFromDirectory(File directory, String packageToScan) throws ClassNotFoundException {
+        List<Class> classes = new ArrayList<Class>();
+        File[] files = directory.listFiles();
+        for (File file : files) {
+            if (file.isDirectory()) {
+                classes.addAll(findClassesFromDirectory(file, packageToScan + "." + file.getName()));
+            } else {
+                if (file.getName().endsWith(CLASS_EXTENSION)) {
+                    final String classPath = packageToScan + '.' + file.getName().substring(0, file.getName().length() - CLASS_EXTENSION.length());
+                    classes.add(Class.forName(classPath));
+                }
+            }
+        }
+        return classes;
+    }
+
+    private List<Class> findClassesFromJar(URL url) throws ClassNotFoundException {
+        try {
+            return doFindClassFromJar(url);
+        } catch (IOException e) {
+            LOGGER.error("Can't load mapping from Jar", e);
+        }
+        return Lists.newArrayList();
+    }
+
+    private List<Class> doFindClassFromJar(URL url) throws IOException, ClassNotFoundException {
+        final List<Class> result = Lists.newArrayList();
+        JarURLConnection jarCon = (JarURLConnection) url.openConnection();
+        final JarFile jarFile = jarCon.getJarFile();
+        for (Enumeration<JarEntry> entries = jarFile.entries(); entries.hasMoreElements();) {
+            result.addAll(extractClass(entries));
+        }
+        return result;
+    }
+
+    private List<Class<?>> extractClass(Enumeration<JarEntry> entries) throws ClassNotFoundException {
+        JarEntry entry = entries.nextElement();
+        String entryPath = entry.getName();
+        if(entryPath.startsWith(packageToDirectory()) && entryPath.endsWith(CLASS_EXTENSION)) {
+            final String classPath = entryPath.substring(0, entryPath.length() - CLASS_EXTENSION.length()).replace(File.separator, ".");
+            return Lists.<Class<?>>newArrayList(Class.forName(classPath));
+        }
+        return Lists.newArrayList();
+    }
+
+    public static final String CLASS_EXTENSION = ".class";
+
+    private static final Logger LOGGER = Logger.getLogger(ContextBuilder.class);
+
+    private final String packageToScan;
 }
