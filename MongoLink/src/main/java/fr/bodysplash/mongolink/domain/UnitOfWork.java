0e21505fb3ba773097919f79aa77e6abf7e310c1
david <dumon.david@gmail.com>
2012-02-07 10:27:35 +0100

    concurrence hashmap dans UnitOfWork

--- MongoLink/src/main/java/fr/bodysplash/mongolink/domain/UnitOfWork.java
+++ MongoLink/src/main/java/fr/bodysplash/mongolink/domain/UnitOfWork.java
@@ -83,5 +83,5 @@ public class UnitOfWork {
 
     private MongoSession session;
 
-    private Map<Key, Value> values = Maps.newHashMap();
+    private Map<Key, Value> values = Maps.newConcurrentMap();
 }
