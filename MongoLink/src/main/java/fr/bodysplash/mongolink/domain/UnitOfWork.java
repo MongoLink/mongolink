b7a85dac8e1dcca9c4808c86a9419c817d026ac4
Charles Couillard <charles.couillard@arpinum.fr>
2012-02-19 10:08:23 +0100

    implémentation de la suppression

--- MongoLink/src/main/java/fr/bodysplash/mongolink/domain/UnitOfWork.java
+++ MongoLink/src/main/java/fr/bodysplash/mongolink/domain/UnitOfWork.java
@@ -1,87 +1,94 @@
 package fr.bodysplash.mongolink.domain;
 
+import java.util.Map;
+
 import com.google.common.base.Objects;
 import com.google.common.collect.Maps;
 import com.mongodb.DBObject;
-import fr.bodysplash.mongolink.MongoSession;
 
-import java.util.Map;
+import fr.bodysplash.mongolink.MongoSession;
 
 public class UnitOfWork {
 
-    public UnitOfWork(MongoSession session) {
-        this.session = session;
-    }
+	public UnitOfWork(final MongoSession session) {
+		this.session = session;
+	}
 
-    public void add(Object id, Object entity, DBObject initialValue) {
-        values.put(new Key(entity.getClass(), id), new Value(entity, initialValue));
-    }
+	public void add(final Object id, final Object entity, final DBObject initialValue) {
+		values.put(new Key(entity.getClass(), id), new Value(entity, initialValue));
+	}
 
-    public void flush() {
-        for (Value value : values.values()) {
-            session.update(value.entity);
-        }
-    }
+	public void flush() {
+		for (Value value : values.values()) {
+			session.update(value.entity);
+		}
+	}
 
-    public boolean contains(Class<?> type, Object dbId) {
-        return values.containsKey(new Key(type, dbId));
-    }
+	public boolean contains(final Class<?> type, final Object dbId) {
+		return values.containsKey(new Key(type, dbId));
+	}
 
-    public <T> T getEntity(Class<?> type, Object dbId) {
-        return (T) getValue(type, dbId).entity;
-    }
+	@SuppressWarnings("unchecked")
+	public <T> T getEntity(final Class<?> type, final Object dbId) {
+		return (T) getValue(type, dbId).entity;
+	}
 
-    public DBObject getDBOBject(Class<?> type, Object dbId) {
-        return getValue(type, dbId).initialValue;
-    }
+	public DBObject getDBOBject(final Class<?> type, final Object dbId) {
+		return getValue(type, dbId).initialValue;
+	}
 
-    private Value getValue(Class<?> type, Object dbId) {
-        return values.get(new Key(type, dbId));
-    }
+	private Value getValue(final Class<?> type, final Object dbId) {
+		return values.get(new Key(type, dbId));
+	}
 
-    public void update(Object id, Object element, DBObject update) {
-        values.put(new Key(element.getClass(), id), new Value(element, update));
-    }
+	public void update(final Object id, final Object element, final DBObject update) {
+		values.put(new Key(element.getClass(), id), new Value(element, update));
+	}
 
-    public void clear() {
-        values.clear();
-    }
+	public void clear() {
+		values.clear();
+	}
 
-    private class Value {
+	public void delete(final Object id, final Object element) {
+		values.remove(new Key(element.getClass(), id));
+	}
 
-        private Value(Object entity, DBObject initialValue) {
-            this.entity = entity;
-            this.initialValue = initialValue;
-        }
+	private class Value {
 
-        Object entity;
-        DBObject initialValue;
-    }
+		private Value(final Object entity, final DBObject initialValue) {
+			this.entity = entity;
+			this.initialValue = initialValue;
+		}
 
-    private class Key {
+		Object entity;
+		DBObject initialValue;
+	}
 
-        private Key(Class<?> type, Object id) {
-            this.type = type;
-            this.id = id;
-        }
+	private class Key {
 
-        @Override
-        public boolean equals(Object o) {
-            Key other = (Key) o;
-            return Objects.equal(type, other.type) && Objects.equal(id, other.id);
-        }
+		private Key(final Class<?> type, final Object id) {
+			this.type = type;
+			this.id = id;
+		}
 
-        @Override
-        public int hashCode() {
-            return Objects.hashCode(type, id);
-        }
+		@Override
+		public boolean equals(final Object o) {
+			Key other = (Key) o;
+			return Objects.equal(type, other.type) && Objects.equal(id, other.id);
+		}
 
-        Class<?> type;
+		@Override
+		public int hashCode() {
+			return Objects.hashCode(type, id);
+		}
 
-        Object id;
-    }
+		Class<?> type;
 
-    private MongoSession session;
+		Object id;
+	}
 
-    private Map<Key, Value> values = Maps.newHashMap();
+	private final MongoSession session;
+
+	private final Map<Key, Value> values = Maps.newHashMap();
+
 }
