cebef8c6d05758467c30c4cd57fda747b93738a5
jb <bodysplash@gmail.com>
2013-03-11 07:55:56 +0100

    forgotten file

---
+++ mongolink/src/test/java/org/mongolink/test/entity/FakeChildChildAggregate.java
@@ -0,0 +1,23 @@
+/*
+ * MongoLink, Object Document Mapper for Java and MongoDB
+ *
+ * Copyright (c) 2012, Arpinum or third-party contributors as
+ * indicated by the @author tags
+ *
+ * MongoLink is free software: you can redistribute it and/or modify
+ * it under the terms of the Lesser GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or any later version.
+ *
+ * MongoLink is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Lesser GNU General Public License for more details.
+ *
+ * You should have received a copy of the Lesser GNU General Public License
+ * along with MongoLink.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.mongolink.test.entity;
+
+public class FakeChildChildAggregate extends FakeChildAggregate{
+}
